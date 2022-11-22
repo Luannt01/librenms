c8f30f553da3d633b99004f1b717633247e53e19
Luannt01 <91786292+Luannt01@users.noreply.github.com>
2022-11-22 14:58:44 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -28,11 +28,15 @@ echo "###########################################################"
 apt upgrade -y
 echo "Installing LibreNMS required packages"
 echo "###########################################################"
-apt install acl curl fping git graphviz imagemagick mariadb-client mariadb-server \
-mtr-tiny nginx-full nmap php-cli php-curl php-fpm php-gd php-gmp php-json php-mbstring \
-php-mysql php-snmp php-xml php-zip rrdtool snmp snmpd whois unzip \
-python3-pymysql python3-dotenv python3-redis python3-setuptools python3-systemd python3-pip
-
+apt install software-properties-common
+add-apt-repository universe
+add-apt-repository ppa:ondrej/php
+apt update
+apt install -y acl curl fping git graphviz imagemagick mariadb-client /
+mariadb-server mtr-tiny nginx-full nmap php-cli php-curl php-fpm php-gd /
+php-gmp php-json php-mbstring php-mysql php-snmp php-xml php-zip rrdtool /
+snmp snmpd whois unzip python3-pymysql python3-dotenv python3-redis /
+python3-setuptools python3-systemd python3-pip 
 ##### Add librenms user
 echo "Add librenms user"
 echo "###########################################################"
